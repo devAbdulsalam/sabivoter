4a85c6bc2844fa350c3319dcf26dadc9a18e5abb
my-amtech <ammuftau74@gmail.com>
2023-05-22 14:37:49 +0100

    update componnents

--- app/(site)/me/page.jsx
+++ app/(site)/me/page.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {getServerSession} from 'next-auth/next'
+// import {getServerSession} from 'next-auth/next'
 import Profile from './../../components/user/Profile';
 import { redirect } from 'next/navigation';
 import { authOptions } from '../../../pages/api/auth/[...nextauth]';
 const ProfilePage = async () => {
-	const session = await getServerSession(authOptions)
-	if (!session) {
-		redirect('/signin?callbackUrl=/dashboard/me')
-	}
+	// const session = await getServerSession(authOptions)
+	// if (!session) {
+	// 	redirect('/signin?callbackUrl=/dashboard/me')
+	// }
 	return <Profile />;
 };
 
